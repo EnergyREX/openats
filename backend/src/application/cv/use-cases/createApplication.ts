9d5e3cf26258b66ea652141112e60463c4523bf3
Francisco Javier Martín Fernández <energy.rex.2003@gmail.com>
2026-04-03 15:19:17 +0200

    chores: add plan for next features

---
+++ backend/src/application/cv/use-cases/createApplication.ts
@@ -0,0 +1,8 @@
+export async function createApplication(candidateUuid: string, offerUuid: string) {
+    // Compare with the candidate with the offer.
+
+    // Get related data, such as  
+
+    // Pass through LLM
+        // - It must generate: an score, annotations via llm.
+}
--- backend/src/infrastructure/drizzle/schema/revokedTokens.ts
+++ backend/src/infrastructure/drizzle/schema/revokedTokens.ts
@@ -1,5 +1,4 @@
 import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
-import { users } from "./users.ts";
 
 export const revokedTokens = pgTable('revoked_tokens', {
     token: varchar('token').notNull(),
