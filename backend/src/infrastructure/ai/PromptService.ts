b397d65da7e6f7115393513157924f89be4396a1
EnergyREX <energy.rex.2003@gmail.com>
2026-04-04 10:50:20 +0200

    feat: create PromptService

--- backend/src/infrastructure/ai/PromptService.ts
+++ backend/src/infrastructure/ai/PromptService.ts
@@ -1,25 +1,35 @@
 import fs from 'node:fs'
+import { IPromptService } from 'src/domain/shared/ports/IPromptService.ts';
+import { PromptsResponseType } from 'src/domain/shared/types/PromptsResponseType.js';
 
-export class PromptService {
+export class PromptService implements IPromptService {
 
-    private getFile(filename: string) {
+    private getFilePath(filename: string): string {
         const filePath = `${process.cwd()}/src/infrastructure/ai/prompts/${filename}`
         return filePath
     }
 
-    getParsingPrompts() {
-        const rawPrompt = fs.readFileSync(this.getFile('/task/parsing.txt'), 'utf8');
+    private collectPrompts(name: string): PromptsResponseType {
+        const rawPrompt = fs.readFileSync(this.getFilePath(`/task/${name}.txt`), 'utf8');
         const prompt = rawPrompt
         .trim()
         .replace(/\n+/g, ' ')
         .replace(/\s{2,}/g, ' ')
 
-        const rawSysPrompt = fs.readFileSync(this.getFile('/system/parsing.txt'), 'utf-8')
+        const rawSysPrompt = fs.readFileSync(this.getFilePath(`/system/${name}.txt`), 'utf-8')
         const sysPrompt = rawSysPrompt
         .trim()
         .replace(/\n+/g, ' ')
         .replace(/\s{2,}/g, ' ')
 
         return { prompt: prompt, system: sysPrompt }
+    }
+
+    getParsingPrompts(): PromptsResponseType {
+        return this.collectPrompts('parsing') 
+    }
+
+    getEvaluationPrompts(): PromptsResponseType {
+        return this.collectPrompts('evaluation')
     }
 }
