fe6fc2f031fb02e2d7ec21aca735f04401aa0093
Francisco Javier Martín Fernández <40529173+EnergyREX@users.noreply.github.com>
2026-05-05 21:49:02 +0200

    chores: add more columns in tables

--- backend/src/infrastructure/drizzle/schema/applications.ts
+++ backend/src/infrastructure/drizzle/schema/applications.ts
@@ -1,4 +1,4 @@
-import { boolean, integer, pgTable, text, uuid } from "drizzle-orm/pg-core";
+import { boolean, integer, jsonb, pgTable, text, uuid, varchar } from "drizzle-orm/pg-core";
 import { candidates } from "./candidates.ts";
 import { jobPostings } from "./jobPostings.ts";
 
@@ -7,6 +7,8 @@ export const applications = pgTable('applications', {
     offerUuid: uuid('offer_uuid').notNull().references(() => jobPostings.uuid),
     candidateUuid: uuid('candidate_uuid').notNull().references(() => candidates.uuid),
     score: integer('score'),
-    annotations: text('annotations').array(),
-    isValid: boolean('is_valid'),
+    annotations: jsonb('annotations').array(),
+    discarded: boolean('discarded').default(false),
+    suggestedStatus: varchar('suggested_status'),
+    discardReason: text('discard_reason')
 })
--- backend/src/infrastructure/drizzle/schema/candidates.ts
+++ backend/src/infrastructure/drizzle/schema/candidates.ts
@@ -1,4 +1,4 @@
-import { pgTable, text, uuid, varchar } from "drizzle-orm/pg-core";
+import { jsonb, pgTable, text, uuid, varchar } from "drizzle-orm/pg-core";
 
 export const candidates = pgTable('candidates', {
     uuid: uuid('uuid').notNull().primaryKey(),
@@ -6,7 +6,18 @@ export const candidates = pgTable('candidates', {
     title: varchar('title').notNull(),
     about: text('about').notNull(),
     skills: text('skills').array().notNull(),
-    email: varchar('email'),
+    email: varchar('email').unique(),
     phone: varchar('phone'),
     address: varchar('address'),
+    website: varchar('website'),
+    github: varchar('github'),
+    linkedin: varchar('linkedin'),
+    experience: jsonb('experience'),
+    projects: jsonb('projects'),
+    education: jsonb('education'),
+    certifications: jsonb('certifications'),
+    languages: jsonb('languages'),
+    volunteering: jsonb('volunteering').array(),
+    additionalInfo: jsonb('additional_info').array(),
+    cvUrl: varchar('cv_url')
 })
