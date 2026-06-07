ad3529c3aa64dac0cc962ab5557c9d1c5b1583e7
Francisco Javier Martín Fernández <40529173+EnergyREX@users.noreply.github.com>
2026-06-07 21:55:07 +0200

    feat: rewrite refresh.ts

--- backend/src/application/auth/types/RefreshToken.d.ts
+++ backend/src/application/auth/types/RefreshToken.d.ts
@@ -1,6 +1,7 @@
 export type RefreshToken = {
     uuid: string
     email: string
+    type: 'refresh'
     iat: number
     exp: number
 }
--- backend/src/application/auth/use-cases/refresh.ts
+++ backend/src/application/auth/use-cases/refresh.ts
@@ -1,39 +1,40 @@
-import { FastifyInstance } from "fastify";
-import { Result } from "../../../domain/shared/types/Result.ts";
+import { Err, Ok, Result } from "../../../domain/shared/types/Result.ts";
 import { RefreshError } from "../errors/refresh.error.ts";
 import type { RefreshResult } from "../types/RefreshResult.d.ts"
 import { RefreshToken } from "../types/RefreshToken.js";
 import { IUserRepository } from "../../../domain/users/repositories/IUserRepository.ts";
+import { ITokenService } from "src/application/ports/ITokenService.ts";
+import { IRoleRepository } from "src/domain/users/repositories/IRoleRepository.ts";
 
-export async function refresh(
-    cookie: string, 
-    fastify: FastifyInstance, 
-    repository: IUserRepository
-): Promise<Result<RefreshResult, RefreshError>> {
-    let result: RefreshToken;
+export async function refresh(cookie: string, jwt: ITokenService, repository: IUserRepository, roleRepository: IRoleRepository): Promise<Result<RefreshResult, RefreshError>> {
+    const result = await jwt.verify<RefreshToken>(cookie);
+    if (!result.ok) return Err(result.error)
 
-    console.log(cookie)
+    const tokenData = result.value
+    if (tokenData.type !== 'refresh') return Err({ message: "This is not a refreshToken!", code: "ERR_AUTH_REFRESH_NOT_A_REFRESH" })
 
-    try {
-        result = await fastify.jwt.verify(cookie) as RefreshToken;
-        console.log(result)
-    } catch (err) {
-        return { ok: false, error: { message: "Cookie is not valid!", code: "ERR_AUTH_REFRESH_TOKEN" } };
-    }
+    const isRevoked = await repository.checkRevoked(cookie)
+    if (isRevoked) return Err({ message: "This token was revoked!", code: "ERR_AUTH_REFRESH_INVALIDATED" })
 
-    const invalidated = await repository.invalidateRefresh(cookie, result.iat, result.exp);
-    if (!invalidated.ok) {
-        return { ok: false, error: { message: "Failed to invalidate refresh token", code: "ERR_AUTH_REFRESH_INVALIDATE" } };
-    }
+    const invalidated = await repository.invalidateRefresh(cookie, tokenData.iat, tokenData.exp);
+    if (!invalidated.ok) return Err(invalidated.error);
 
-    const refreshToken = fastify.jwt.sign({
-        uuid: result.uuid, 
-        email: result.email 
+    const dbUserRoles = await roleRepository.getByUserUUID(tokenData.uuid)
+    if (!dbUserRoles.ok) return Err(dbUserRoles.error) 
+    const rolesUuids = dbUserRoles.value.map((r) => r.getUUID() )
+
+    const refreshToken = await jwt.sign({
+        uuid: tokenData.uuid, 
+        email: tokenData.email,
+        type: 'refresh'
     }, { expiresIn: "7d" });
 
-    const accessToken = fastify.jwt.sign({
-        uuid: result.uuid 
+    const accessToken = await jwt.sign({
+        uuid: tokenData.uuid,
+        roles: rolesUuids,
+        type: 'access'
     }, { expiresIn: "30m" });
 
-    return { ok: true, value: { refreshToken, accessToken } };
+
+    return Ok({ refreshToken, accessToken});
 }
