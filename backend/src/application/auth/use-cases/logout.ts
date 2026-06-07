86c225f3fedb419c9b12659be88aa834af28d8d4
Francisco Javier Martín Fernández <40529173+EnergyREX@users.noreply.github.com>
2026-06-07 21:42:34 +0200

    fix: rewrite logout function

--- backend/src/application/auth/use-cases/logout.ts
+++ backend/src/application/auth/use-cases/logout.ts
@@ -1,32 +1,16 @@
-import { FastifyInstance } from "fastify"
 import { IUserRepository } from "../../../domain/users/repositories/IUserRepository.ts" 
-import { Result } from "../../../domain/shared/types/Result.ts"
+import { Err, Ok, Result } from "../../../domain/shared/types/Result.ts"
 import { LogoutError } from "../errors/logout.error.ts"
 import { RefreshToken } from "../types/RefreshToken.js"
+import { ITokenService } from "src/application/ports/ITokenService.ts"
 
-export async function logout
-(cookie: string, fastify: FastifyInstance, userRepository: IUserRepository): Promise<Result<void, LogoutError>> {
-
-  // Check if the token is legit
-  
-  try {
-    fastify.jwt.verify(cookie)
-
-  } catch (err) {
-    return {
-      ok: false,
-      error: { message: "This cookie is not valid!", code: "ERR_USER_LOGOUT_COOKIE_NOT_VALID" }
-    }
-  }
-
-  const cookieData = fastify.jwt.decode(cookie) as RefreshToken
-
+export async function logout (cookie: string, jwt: ITokenService, userRepository: IUserRepository): Promise<Result<void, LogoutError>> {
+  const token = await jwt.verify<RefreshToken>(cookie)
+  if (!token.ok) return Err(token.error) 
+  const tokenData = token.value
   // If legit, invalidate in database.
-  const result = await userRepository.invalidateRefresh(cookie, cookieData.iat, cookieData.exp)
+  const result = await userRepository.invalidateRefresh(cookie, tokenData.iat, tokenData.exp)
+  if (!result.ok) return Err(result.error)
 
-  if (result.ok) {
-    return { ok: true, value: undefined }
-  } else {
-    return { ok: false, error: { message: result.error.message, code: "ERR_USER_LOGOUT_INFRASTRUCTURE" } }
-  }
+  return Ok(undefined)
 }
