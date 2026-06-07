cd049d6409e3d2a5e1e85505fbdd84bebb359db9
Francisco Javier Martín Fernández <40529173+EnergyREX@users.noreply.github.com>
2026-06-07 21:36:56 +0200

    chores: set uuid property as its VO

--- backend/src/domain/users/aggregates/User.ts
+++ backend/src/domain/users/aggregates/User.ts
@@ -3,13 +3,13 @@ import { Email } from "../value-objects/Email.ts"
 import { Password } from "../value-objects/Password.ts"
 
 export class User {
-  private readonly uuid: string
+  private readonly uuid: UUID
   private readonly name: string
   private readonly email: Email
   private readonly password: Password
   private readonly verified: boolean
 
-  constructor(uuid: string, name: string, email: Email, password: Password, verified: boolean) {
+  constructor(uuid: UUID, name: string, email: Email, password: Password, verified: boolean) {
       this.uuid = uuid
       this.name = name
       this.email = email
@@ -17,18 +17,22 @@ export class User {
       this.verified = verified
   }
 
-  static create(uuid: string, name: string, email: string, password: string, verified: boolean): User {
-        
+  static create(uuid: string, name: string, email: string, password: string, verified: boolean): User {  
     const userEmail = new Email(email)
         const userPassword = new Password(password)
 
-        const user = new User(uuid as UUID, name, userEmail, userPassword, verified) 
+        const user = new User(
+          new UUID(uuid), 
+          name, 
+          userEmail, 
+          userPassword, 
+          verified) 
 
         return user
     }
 
   getUser() { return this }
-  getUUID(): string { return this.uuid }
+  getUUID(): UUID { return this.uuid }
   getName(): string { return this.name }
   getEmail(): string { return this.email.getValue() }
   getPassword(): string { return this.password.getValue() }
