f668d28568fcc3bef06450ed9e339ef6c491fec2
Francisco Javier Martín Fernández <40529173+EnergyREX@users.noreply.github.com>
2026-06-07 21:51:21 +0200

    feat: delete dependency of bcrypt and crypto, use Err and Ok

--- backend/src/application/auth/use-cases/register.ts
+++ backend/src/application/auth/use-cases/register.ts
@@ -1,41 +1,37 @@
-import { Result } from "../../../domain/shared/types/Result.ts";
+import { IPasswordHasher } from "src/application/ports/IPasswordHasher.ts";
+import { Err, Ok, Result } from "../../../domain/shared/types/Result.ts";
 import { IUserRepository } from "../../../domain/users/repositories/IUserRepository.ts";
-import bcrypt from 'bcrypt'
-import crypto from 'crypto'
 import { RegisterError } from "../errors/register.error.ts";
 import { User } from "src/domain/users/aggregates/User.ts";
+import { IMailService } from "src/application/ports/IMailService.ts";
+import { toCommonErrorHandle } from "src/domain/shared/helpers/ToCommonErrorHandle.ts";
 
-export async function register(
-    name: string, email: string, password: string, repository: IUserRepository): 
-    Promise<Result<void, RegisterError>> {
+export async function register(name: string, email: string, password: string, 
+    repository: IUserRepository, hasher: IPasswordHasher, mailService: IMailService): Promise<Result<void, RegisterError>> {
 
     const existEmail = await repository.getByEmail(email)
     
     if (existEmail.ok) {
-        return { ok: false, error: { message: "Email already exists!", code: "ERR_USER_ALREADY_EXISTS" } }
+        return Err({ message: "Email already exists!", code: "ERR_USER_ALREADY_EXISTS" })
     }
     
     try {
         const uuid = crypto.randomUUID()
-        const hashedPassword = await bcrypt.hash(password, 12)
+        const hashedPassword = await hasher.hash(password)
         const user = await User.create(uuid, name, email, hashedPassword, false)
-        const verificationCode = await crypto.randomBytes(32).toString('base64url')
+        const verificationCode = await hasher.generateVerificationCode()
 
         const saveResult = await repository.save(user, verificationCode)
 
         if (!saveResult.ok) {
-            return { ok: false, error: {
-                message: saveResult.error.message,
-                code: "ERR_USER_REGISTER_FAILED"
-            }}
+            return Err({ message: saveResult.error.message, code: "ERR_USER_REGISTER_FAILED" })
         }
 
-        return { ok: true, value: undefined }
+        //* Still needs some work, such as adding a template and put inside an email.
+        mailService.sendMail('no-reply', user.getEmail(), "Your verification code", `Your verification code is ${verificationCode}. It will expire in 24h.` )
+
+        return Ok(undefined)
     } catch (err) {
-        if (err instanceof Error) {
-            return { ok: false, error: { message: err.message, code: "ERR_USER_REGISTER"}}
-        } else {
-            return { ok: false, error: { message: "Unknown error", code: "ERR_USER_REGISTER" }}
-        }
+        return Err(toCommonErrorHandle(err, 'ERR_USER_REGISTER_FAILED'))
     }
 }
